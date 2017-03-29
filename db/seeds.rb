f0f7f7965504c28bcd96061d64e843bfb3c287ca
Ryan Arakawa <arakawa.ryan@gmail.com>
2017-03-29 00:46:29 +0000

    Checkpoint 22 Assignment: Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,17 @@ posts = Post.all
     )
 end
 
+10.times do
+    SponsoredPost.create!(
+        topic: topics.sample,
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph,
+        price: rand(1..500)
+    )
+end
+
 puts "Seed Finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsored posts created"
